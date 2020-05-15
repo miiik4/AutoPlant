cec8f2bd0df5704377845041c754237c255fa31f
Mikkel Holmberg <31065498+miiik4@users.noreply.github.com>
2020-05-15 12:50:58 +0200

    updates

--- AutoPlant.ino
+++ AutoPlant.ino
@@ -1,15 +1,34 @@
+// importer biblioteker
 #include <LiquidCrystal_I2C.h>
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+ThreeWire myWire(6,7,8);
+RtcDS1302<ThreeWire> Rtc(myWire);
 
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// definerer alle verdier
 int encoderPosCount = 0; 
-int pinALast;  
+int pinALast;
 int aVal;
 boolean btn;
 int selected = 0;
 
-int hum = 0;
-int light = 0;
+unsigned long pumpTime = 0;
+unsigned long checkTime = 0;
+
+int currentHour;
+int targetHour = 0;
+int lightDuration = 0;
+int startTime = 8; // kl. 0800
+boolean lightStatus = false;
+
+int targetHum = 0;
+int failSafePump = 0;
+
+// testverdi
+int trueHum = 20;
 
 void setup() { 
   pinMode(2, INPUT);
@@ -17,13 +36,13 @@ void setup() {
   pinMode(4, INPUT);
    
   lcd.begin(16, 2);
+  
   lcd.setCursor(1, 0);
   lcd.print("Fuktighet:");
-
   lcd.setCursor(1,1);
   lcd.print("Lysmengde:");
 
-  // startverdier
+  // startverdier for UI
   lcd.setCursor(0,0);
   lcd.print(">");
   lcd.setCursor(12,0);
@@ -34,14 +53,69 @@ void setup() {
   lcd.print("t");
    
   pinALast = digitalRead(2);
+
+  // henter starttid
+  RtcDateTime checkTime = Rtc.GetDateTime();
+  currentHour = checkTime.Hour();
+    
   Serial.begin (9600);
  } 
 
 void loop() {
+
+  /* Lys */
+  
+  // sjekker hvilken time det er hvert 10 minutt
+  if(millis() >= checkTime + 100000){
+    checkTime += 100000;
+    RtcDateTime checkTime = Rtc.GetDateTime();
+    currentHour = checkTime.Hour();
+  }
+
+  // fikser problemet med 24timers klokke
+  targetHour = startTime+lightDuration;
+  if(currentHour == 0){
+    targetHour = targetHour - 24;
+  } else if(currentHour == startTime){
+    targetHour = targetHour + 24;
+  }
+
+  // bytter status på lys basert på tid og status
+  if(targetHour > currentHour && lightStatus == false){
+    lightStatus = true;
+    Serial.println("Lights ON");
+  } else if(targetHour <= currentHour && lightStatus == true) {
+    lightStatus = false;
+    Serial.println("Light Off");
+  } /* Slutt lys */
+
+  /* vanning */
+  
+  // stopper programmet om pumpe kjører mer enn 6 ganger/ 1 minutt
+  if(failSafePump > 6){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("FailSafe: Pumpe");
+    delay(0xFFFFFFFF);
+  }
+
+  // venter 10sek før den kjører pumpe på ny
+  if(targetHum > trueHum){
+    if(millis() >= pumpTime + 10000){
+    pumpTime += 10000;
+    Serial.println("run pump...");
+    failSafePump++;
+    }
+  } else {
+    failSafePump = 0;
+  } /* slutt vanning */
+
+  /* program UI */
+  
   aVal = digitalRead(2);
   btn = digitalRead(4);
 
-  // her velger man og printer riktig valg
+  // Valg av meny printer riktig valg
   if(btn == LOW){ 
     if(selected == 1){
       selected = 0;
@@ -62,29 +136,28 @@ void loop() {
 
   if(selected == 0){
     if (aVal != pinALast){
-      if (digitalRead(3) != aVal && hum < 100) {
-      hum ++;
-      } else if(digitalRead(3) == aVal && hum > 0) {
-      hum--;
+      if (digitalRead(3) != aVal && targetHum < 100) {
+      targetHum ++;
+      } else if(digitalRead(3) == aVal && targetHum > 0) {
+      targetHum--;
       }
     lcd.setCursor(12,0);
-    lcd.print(hum);
+    lcd.print(targetHum);
     lcd.print("%  ");
     } 
     pinALast = aVal;
-
     
   } else if(selected == 1) {
     if (aVal != pinALast){
-      if (digitalRead(3) != aVal && light < 24) {
-      light ++;
-      } else if(digitalRead(3) == aVal && light > 0) {
-      light--;
+      if (digitalRead(3) != aVal && lightDuration < 24) {
+      lightDuration ++;
+      } else if(digitalRead(3) == aVal && lightDuration > 0) {
+      lightDuration--;
       }
     lcd.setCursor(12,1);
-    lcd.print(light);
+    lcd.print(lightDuration);
     lcd.print("t ");
     } 
     pinALast = aVal;
-  }
-} 
+  } /* Slutt program UI */
+}
